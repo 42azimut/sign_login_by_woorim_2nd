a2cec6062721f5a7a3012588f3fc1cdd468ec544
42azimut <azimutuniverse@gmail.com>
2021-03-25 22:36:40 +0900

    25 회원가입 라우팅 기능 구현 in 서버 _ git tag

--- login-lecture/app/src/models/UserStorage.js
+++ login-lecture/app/src/models/UserStorage.js
@@ -21,12 +21,20 @@ class UserStorage {
     static getUserInfo(id) {
         const users = this.#users;
         const idx = users.id.indexOf(id);
-        const userKeys = Object.keys(users)
+        const userKeys = Object.keys(users);  // => [id, psword, name]
         const userInfo = userKeys.reduce((newUser, info) => {
             newUser[info] = users[info][idx];
             return newUser;
         }, {});
         return userInfo;
+    }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.psword.push(userInfo.psword);
+        console.log(users);
     }
 }
 
