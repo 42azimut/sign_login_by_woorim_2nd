d90c3208793a0cc1f4488e0ad40c8a4912c23216
Jayden Jeong <azimutuniverse@gmail.com>
2021-03-31 01:01:06 +0900

    Update db.js

--- login-lecture/app/src/config/db.js
+++ login-lecture/app/src/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "login-lecture-azimut.cbw1mxub6jhp.ap-northeast-2.rds.amazonaws.com",
+    host: "login-lectu********2.rds.amazonaws.com",
     user: "admin",
     password: "******",
     database: "login_lecture",
